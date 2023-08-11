d4c1971b38d808e173a864152ec8802653fb2097
jurazil <jurazilgreen@gmail.com>
2023-08-10 23:22:00 -0700

    displaying news

--- lib/services/news_provider.dart
+++ lib/services/news_provider.dart
@@ -7,12 +7,12 @@ import 'package:news_app/model/news.dart';
 class NewsProvider {
   static Future<News> fetchNews() async {
     final String uri =
-        "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=7673b72b5f884a87a39b1e6296506152";
+        "https://newsapi.org/v2/top-headlines/sources?apiKey=7673b72b5f884a87a39b1e6296506152";
     Response response = await get(Uri.parse(uri));
     Map data = jsonDecode(response.body);
     List articles = data["articles"];
     final newRandom = Random();
-    var article = articles[newRandom.nextInt(9)];
+    var article = articles[newRandom.nextInt(articles.length)];
     News news = News.fromMap(article);
     return news;
   }
